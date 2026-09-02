8d7576eab6297420010c69191d43dda232572a59
elChivoR <rderdev@gmail.com>
2026-09-02 12:18:48 -0400

    chore: solution + SPT_Runtime build tree, move ModInfo to Shared, bump 1.1.0

--- Shared/ModInfo.cs
+++ Shared/ModInfo.cs
@@ -3,7 +3,7 @@ namespace KappaTracker
     /// <summary>Single source of truth for the mod version + a few shared constants.</summary>
     public static class ModInfo
     {
-        public const string Version = "1.0.1";
+        public const string Version = "1.1.0";
 
         /// <summary>"Secure container Kappa" item template id.</summary>
         public const string KappaContainerTpl = "5c093ca986f7740a1867ab12";
